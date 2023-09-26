0f752e9e13d9bc6afb13db81fb113acbd3401283
Lucas dos Reis Viana <55816219+Lucas-vna@users.noreply.github.com>
2023-09-26 15:02:58 -0300

    Update Ex prova antg - desafio.c

--- Ex prova antg - desafio.c
+++ Ex prova antg - desafio.c
@@ -9,17 +9,19 @@ char aux;
 
 void handlerAL(int signum){
 	write(1, "\nSaindo do Programa...\n", 23);
+	exit(0);
 }
 
 void handlerINT(int signum){
 	count++;
 	write(1, "\nctrl+c apertado", 16);
+	
 	if(count == 3){
         write(1, "Realmente Deseja Sair? [Y/N]: ", 31);
         alarm(20);
 
         read(0, &aux, sizeof(aux));
-        signal(SIGALRM, handlerAL);
+        //signal(SIGALRM, handlerAL);
 
          if (aux == 'y')
          {
@@ -30,10 +32,19 @@ void handlerINT(int signum){
             printf("2");
             count = 0;
          }
-        exit(0);
+        
     };
 }
 
+void handlesrTSTP(int signum){
+
+	int iaux = count + '0';
+
+	write(1, "\nctrl+c apertado ", 18);
+	write(1, &aux, 1);
+	write(1, " vezes\n", 8);
+	
+}
 
 int main (){
 
@@ -42,7 +53,6 @@ int main (){
 	signal(SIGINT, handlerINT);
 
     while(1){
-        alarm(5);
         pause();
     }
 
